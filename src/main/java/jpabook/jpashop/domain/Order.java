a3c692c554981346030cb3e7bb0c23d7a98c7f11
dodeon <sojeongw.official@gmail.com>
2021-04-26 06:20:18 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,13 +18,17 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "orders") // DB에 order by가 예약어로 걸려있어서 테이블명을 따로 지정해준다.
+// DB에 order by가 예약어로 걸려있어서 테이블명을 따로 지정해준다.
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
   @Id
@@ -64,5 +68,48 @@ public class Order {
   public void setDelivery(Delivery delivery) {
     this.delivery = delivery;
     delivery.setOrder(this);
+  }
+
+  // 생성 메서드
+  public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+    Order order = new Order();
+    order.setMember(member);
+    order.setDelivery(delivery);
+
+    for (OrderItem orderItem : orderItems) {
+      order.addOrderItem(orderItem);
+    }
+
+    order.setStatus(OrderStatus.ORDER);
+    order.setOrderDate(LocalDateTime.now());
+
+    return order;
+  }
+
+  // 비즈니스 로직
+  /**
+   * 주문 취소
+   */
+  public void cancel() {
+    if(delivery.getStatus() == DeliveryStatus.COMP) {
+      throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+    }
+
+    this.setStatus(OrderStatus.CANCEL);
+
+    // IDE 에디터 색때문에 정말 this를 꼭 명시적으로 보여줘야할 때가 아니면
+    // this.orderItems보다 orderItems를 선호한다.
+    for (OrderItem orderItem : orderItems) {
+      // 여러 상품을 주문했을 때 다 주문 취소를 해줘야 한다.
+      orderItem.cancel();
+    }
+  }
+
+  // 조회 로직
+  /**
+   * 전체 주문 가격 조회
+   */
+  public int getTotalPrice() {
+    return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
   }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -9,12 +9,15 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
   @Id
@@ -33,4 +36,30 @@ public class OrderItem {
   private int orderPrice;
 
   private int count;
+
+  // 생성 메서드
+  public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+    // 쿠폰 등의 가격 변경 가능성 때문에 따로 만든다.
+    OrderItem orderItem = new OrderItem();
+
+    orderItem.setItem(item);
+    orderItem.setOrderPrice(orderPrice);
+    orderItem.setCount(count);
+
+    // 넘어온 것만큼 재고를 뺀다.
+    item.removeStock(count);
+
+    return orderItem;
+  }
+
+  // 비즈니스 로직
+  public void cancel() {
+    // 해당 아이템에 대한 주문 수량을 취소한 만큼 원복시켜야 한다.
+    getItem().addStock(count);
+  }
+
+  // 주문 가격과 수량이 orderItem에 있으므로 여기서 가격을 계산한다.
+  public int getTotalPrice() {
+    return getOrderPrice() * getCount();
+  }
 }
