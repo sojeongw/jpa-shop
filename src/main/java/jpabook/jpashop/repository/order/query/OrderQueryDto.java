e733a466453dafeda9233e404470aed54af5abd5
sojeongw <sojeongw.official@gmail.com>
2022-05-01 15:37:37 +0900

    컬렉션 조회 최적화: JPA에서 DTO 직접 조회

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.repository.order.query;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class OrderQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    // 컬렉션은 생성자에서 제외한다.
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
